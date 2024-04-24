0e10fe18f93f5b0e6750bbab64c1884ebaa0394f
esther odedoyin <estherodedoyin1@example.com>
2024-04-24 15:49:05 +0100

    first commit

---
+++ clientside/tailwind.config.js
@@ -0,0 +1,23 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"
+    ],
+
+  theme: {
+    // colors: {
+    //   twitterblue: "rgb(29,155,240)",
+    //   navyblue: "#1f2937",
+    //   white: "#ffffff",
+    //   ash: "#adaf9b",
+    //   black: "#000000",
+    //   blue:"#003873"
+    // },
+    extend: {},
+    variants: {},
+    plugins: [],
+  },
+  plugins: [],
+}
+
+
